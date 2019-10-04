89b948623fa83738a337f1ba0a70e0a600f70697
IG-22\user <bitvalser@gmail.com>
2019-10-04 13:54:17 +0300

    fix

--- src/android/NavBar.java
+++ src/android/NavBar.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.util.Log;
 import android.view.View;
 import android.os.Handler;
+import android.content.res.Resources;
 import java.lang.reflect.Method;
 
 public class NavBar extends CordovaPlugin {
